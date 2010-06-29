43b2fc00e36b71fb6da7bc475b3d70a21acbb622
Jakob Ovrum <jakobovrum@gmail.com>
2010-06-30 05:56:35 +0900

    added util.lua

---
+++ util.lua
@@ -0,0 +1,15 @@
+local type = type
+local error = error
+local getinfo = debug.getinfo
+
+module "luamvc.util"
+
+function checkArg(argn, expectedType, arg)
+    local t = type(arg)
+    if t ~= expectedType then
+        local name = getinfo(2, "n").name
+        error(("bad argument #%d to %s (%s expected, got %s)"):format(argn, name, expectedType, t), 3)
+    end
+
+    return arg
+end
